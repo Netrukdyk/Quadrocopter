104c750575ff524936482e51e80319a4f4adf6de
Netrukdyk <bekraujis@gmail.com>
2014-10-11 14:51:21 +0300

    Main

---
+++ src/com/quadrocopter/Vector3.java
@@ -0,0 +1,24 @@
+package com.quadrocopter;
+
+public class Vector3 {
+	public double x;
+	public double y;
+	public double z;
+	
+	public Vector3(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public void normalize() {
+		double l = Math.sqrt(x*x + y*y + z*z);
+		x /= l;
+		y /= l;
+		z /= l;
+	}
+	
+	public String toString() {
+		return String.format("V(%.3f,%.3f,%.3f)", x, y, z);
+	}
+}
